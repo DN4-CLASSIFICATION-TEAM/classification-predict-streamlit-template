6ad72da6a031097880708ea39db8c278b8c5a75e
Pertunia <113897328+PertuniaKele@users.noreply.github.com>
2023-06-28 14:40:42 +0200

    Update home.py

--- home.py
+++ home.py
@@ -24,14 +24,15 @@
 import streamlit as st
 from PIL import Image
 import joblib
-# from pages import home, modeling, aboutUsupdate
+# from pages import home, modeling, aboutUsupdate, aboutTheApp
 
 # Create a dictionary to map page names to their respective modules
-# pages = {
-#     "Home": home,
-#     "Modeling": modeling,
-#     "about Us": aboutUsupdate,
-# }
+pages = {
+    "Home": home,
+    "About the App":aboutTheApp,
+    "Modeling": modeling,
+    "about Us": aboutUsupdate,
+}
 
 # The main function where we will build the actual app
 def main():
@@ -39,15 +40,12 @@ def main():
     image = Image.open("./resources/imgs/dn4logo.png")
     st.sidebar.image(image, width=200)
     st.title("Welcome to Sense Solutions!")
-
-    # Create a two-column layout
-    col1, col2 = st.columns(2)
-    col1.image(image)
-    col2.markdown("#### We are delighted to present our state-of-the-art web application designed for tweet classification. With our innovative technology, you can effortlessly analyze and categorize tweets based on their sentiments, providing you with valuable insights into public opinions")
-    st.info("Our tweet classification web app utilizes advanced machine learning algorithms to accurately determine the sentiment of each tweet. Whether it's positive, negative, or neutral, our model can swiftly classify the sentiment, allowing you to gain a comprehensive understanding of public sentiment on various topics.")    
-    image = Image.open("./resources/imgs/chatBot.png")
-    st.image(image)
-
+    st.markdown("<h2 style='text-align: center;'>Let's Classify</h2>", unsafe_allow_html=True)
+    wel = Image.open("resources/imgs/chatBot.png")
+    st.image(wel,use_column_width=True, clamp=False, width = 200, output_format="PNG")
+    st.markdown("#### We are delighted to present our state-of-the-art web application designed for tweet classification. With our innovative technology, you can effortlessly analyze and categorize tweets based on their sentiments, providing you with valuable insights into public opinions")
+    st.write("Stand Out from the Competition: By using our sentiment analysis model, you'll have an advantage over others because you can respond quickly to public sentiment, address concerns, and align your efforts with what people are saying.")
+    st.write("Support for Decision Making: Our model acts as a reliable tool to help you make decisions based on data. You can use the insights to guide your marketing strategies, improve your products, and handle any crises that may arise.")
 
 # Required to let Streamlit instantiate our web app.
 if __name__ == '__main__':
